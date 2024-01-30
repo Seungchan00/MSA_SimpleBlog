6b76ac8da723ab014bdea1603d3fc973f030bc76
Seungchan00 <jscpys1@naver.com>
2024-01-31 01:21:40 +0900

    master merge

--- post/src/main/java/blog/domain/Post.java
+++ post/src/main/java/blog/domain/Post.java
@@ -1,24 +1,19 @@
 package blog.domain;
 
 import blog.PostApplication;
-import blog.domain.PostCreated;
-import blog.domain.PostDeleted;
-import blog.domain.PostEdited;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 import javax.persistence.*;
-import javax.transaction.Transactional;
+
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.Data;
 
 @Entity
 @Table(name = "Post_table")
 @Data
-//<<< DDD / Aggregate Root
 public class Post {
 
     @Id
@@ -56,43 +51,37 @@ public class Post {
 
     public static PostRepository repository() {
         PostRepository postRepository = PostApplication.applicationContext.getBean(
-            PostRepository.class
-        );
+                PostRepository.class);
         return postRepository;
     }
 
-    
-
+    // 진승찬 : 게시글에 댓글이 달렸을 때, 게시글의 댓글리스트 필드에 댓글 추가하는 메서드
     @Transactional
     public static void commentCreateOnPost(CommentCreated commentCreated) {
-        
-        repository().findById(commentCreated.getPostId()).ifPresent(post->{ // repository에서 postId 와 일치하는 값을 post에 가져온다.  
-            
-            List<Long> newCommentList = new ArrayList<>(post.getCommentList());     // 새로운 arrayList에 생성하고, 기존에 있던 commnetList를 넣는다. 
-            newCommentList.add(commentCreated.getUserId());                             // newCommenList리스트에 commectCreated의id를 추가한다.
-            post.setCommentList(newCommentList);                                    // 기존의 commentList를 newCommentList로 set한다.
-            
-            repository().save(post);                           // 저장한다.
+        repository().findById(commentCreated.getPostId()).ifPresent(post -> { 
 
-         });
-
-    }
-
-    //>>> Clean Arch / Port Method
-    //<<< Clean Arch / Port Method
-    @Transactional
-    public static void commentDeleteOnPost(CommentDeleted commentDeleted) {
-     
-        
-        repository().findById(commentDeleted.getPostId()).ifPresent(post->{
-            
             List<Long> newCommentList = new ArrayList<>(post.getCommentList());
-            newCommentList.remove(commentDeleted.getUserId());
-            post.setCommentList(newCommentList);  
+                                                                                
+            newCommentList.add(commentCreated.getId()); 
+            post.setCommentList(newCommentList); 
+
             repository().save(post);
 
-         });
+        });
+    }
 
+    // 진승찬 : 게시글에 댓글이 삭제되었을 때, 게시글의 댓글리스트 필드에 댓글을 삭제하는 메서드
+    @Transactional
+    public static void commentDeleteOnPost(CommentDeleted commentDeleted) {
+
+        repository().findById(commentDeleted.getId()).ifPresent(post -> {
+
+            List<Long> newCommentList = new ArrayList<>(post.getCommentList());
+            newCommentList.remove(commentDeleted.getId());
+            post.setCommentList(newCommentList);
+            repository().save(post);
+
+        });
     }
 
     // 안치윤 : 유저 정보가 업데이트 되었을 때, 게시글 정보도 업데이트하는 메서드
@@ -106,7 +95,7 @@ public class Post {
                 throw new NoSuchElementException("Can't find user with id " + userUpdated.getId());
             }
 
-            // 게시글 객체의 닉네임 부분을 수정된 닉네임으로 변경하고 이를 DB에 저장합니다 
+            // 게시글 객체의 닉네임 부분을 수정된 닉네임으로 변경하고 이를 DB에 저장합니다
             for (Post new_post : post_list) {
                 new_post.setNickname(userUpdated.getNickname());
                 repository().save(new_post);
